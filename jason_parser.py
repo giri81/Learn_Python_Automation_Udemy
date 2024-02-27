e759dd54c832fc47f117f518c73c46a3916c7ffc
Girish Devappa <sd.girish@gmail.com>
2024-02-27 13:06:17 +0530

    Add files via upload

--- jason_parser.py
+++ jason_parser.py
@@ -27,6 +27,34 @@ def print_json_keys(file_path):
         for key in keys:
             print(key)
 
+
 # Specify the path to the complex JSON file
 file_path = 'static_file.json'
 print_json_keys(file_path)
+
+
+# Print the price of a title ie one among the many element in the json file
+# Given the key as input
+def fetch_price(json_file, title):
+    with open(json_file, 'r') as f:
+        data = json.load(f)
+    courses = data.get('course_data', [])
+    """"The get() method retrieves the value for the specified key ('course_data' 
+    in this case) from the dictionary (data), and if the key is not found, it returns 
+    the default value provided as the second argument, which in this case is an empty
+     list ([]). 
+    """
+
+    for course in courses:
+        if course['title'] == title:
+            return course['price']
+    return None
+
+
+json_file = 'static_file_2.json'
+title = input("Enter the title of the course: ")
+price = fetch_price(json_file, title)
+if price is not None:
+    print(f"The price of '{title}' is: {price}")
+else:
+    print(f"Course '{title}' not found.")
